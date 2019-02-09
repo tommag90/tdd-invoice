deb7999f8dc899a7cecd53dcb07639e4e1c60afb
Tomek <student11@pcoip.ki.agh.edu.pl>
2019-02-09 12:17:04 +0100

    Some of Invoice tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,50 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
+
+
+	private Collection<Product> products = new ArrayList<Product>();
 
 	public void addProduct(Product product) {
-		// TODO: implement
+		//this.products.add(product);
+		this.addProduct(product, 1);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		for (int i = 0; i < quantity; i++) {
+			this.products.add(product);
+		}
 	}
 
-	public BigDecimal getSubtotal() {
-		return null;
+	public BigDecimal getTotalNetPrice() {
+		BigDecimal sum = BigDecimal.ZERO;
+		for (Product product : this.products) {
+			sum = sum.add(product.getPrice());
+		}
+		return sum;
 	}
 
 	public BigDecimal getTax() {
-		return null;
+		if (products.size() == 0) {
+			return BigDecimal.ZERO;
+		}
+		else {
+			return null;
+		}
 	}
 
-	public BigDecimal getTotal() {
-		return null;
+	public BigDecimal getTotalGrossPrice() {
+		if (products.size() == 0) {
+			return BigDecimal.ZERO;
+		}
+		else {
+			return null;
+		}
 	}
 }
